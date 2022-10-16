5b263495871407ff1da6f7355ede45581a4ae9fa
Benny Burkert <benny.burkert@codecentric.de>
2022-10-16 18:04:43 +0200

    Loading and displaying content from API

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const apiUrl =
+  "https://friendifyv2.azurewebsites.net/api/drei-fragezeichen?limit=180&code=Qw39cXFJF7FuwKBhqUuZ";
+let spotifyUrl = "";
+
+window.onload = () => {
+  fetch(apiUrl, {
+    method: "get",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => fillApiContent(data))
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+function fillApiContent(content) {
+  console.log(content);
+  const titleTag = document.getElementById("episode-title");
+  const releaseTag = document.getElementById("episode-release-date");
+  const image = document.getElementById("episode-img");
+
+  titleTag.innerText = content.name;
+  releaseTag.innerText = content.release_date;
+  image.src = content.images[0].url;
+  spotifyUrl = content.link;
+}
+
+function openInSpotify() {
+  window.open(spotifyUrl, "_blank");
+}
